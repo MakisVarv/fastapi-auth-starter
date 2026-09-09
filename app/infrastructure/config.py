e61cdcfa466afb4d18921b8f1fc0f6f0c99fdf82
MakisVarv <christomvarv@yahoo.com>
2026-09-09 18:14:02 +0300

    jwt config

--- app/infrastructure/config.py
+++ app/infrastructure/config.py
@@ -4,6 +4,10 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     DATABASE_URL: str
 
+    JWT_SECRET_KEY: str
+    JWT_ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRES_MINUTES: int = 15
+
     model_config = SettingsConfigDict(env_file=".env")
 
 
