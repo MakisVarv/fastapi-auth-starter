693bdf16d25303c5e5ac0d7a77d491b1227ba7cc
MakisVarv <christomvarv@yahoo.com>
2026-09-17 16:23:47 +0300

    Update role.py

--- app/domain/entities/role.py
+++ app/domain/entities/role.py
@@ -11,3 +11,8 @@ class Role:
     description: str | None = None
     id: UUID = field(default_factory=uuid4)
     permissions: list[Permission] = field(default_factory=list)
+
+    def has_permission(self, permission_name: str) -> bool:
+        return any(
+            permission.name == permission_name for permission in self.permissions
+        )
