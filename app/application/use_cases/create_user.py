bc044a3e4d286f6f1cfbb6fedb88d6f7c259c5b9
MakarosVar <christomvarv@yahoo.com>
2026-09-19 17:19:05 +0300

    Create create_user.py

---
+++ app/application/use_cases/create_user.py
@@ -0,0 +1,54 @@
+from uuid import UUID
+
+from app.application.errors import (
+    EmailAlreadyRegisteredError,
+    RoleNotFoundError,
+)
+from app.application.ports.password_hasher import PasswordHasher
+from app.application.ports.unit_of_work import UnitOfWork
+from app.domain.entities.user import User
+
+
+class CreateUser:
+    def __init__(
+        self,
+        uow: UnitOfWork,
+        password_hasher: PasswordHasher,
+    ) -> None:
+        self.uow = uow
+        self.password_hasher = password_hasher
+
+    def execute(
+        self,
+        first_name: str,
+        last_name: str,
+        email: str,
+        password: str,
+        role_id: UUID,
+        phone: str | None = None,
+    ) -> User:
+        email = email.strip().lower()
+        with self.uow:
+            existing_user = self.uow.users.get_by_email(email)
+            role = self.uow.roles.get_by_id(role_id)
+            if role is None:
+                raise RoleNotFoundError
+            if existing_user is not None:
+                raise EmailAlreadyRegisteredError
+
+            password_hash = self.password_hasher.hash(password)
+
+            user = User(
+                first_name=first_name,
+                last_name=last_name,
+                email=email,
+                role=role,
+                phone=phone,
+                password_hash=password_hash,
+            )
+
+            self.uow.users.add(user)
+
+            self.uow.commit()
+
+            return user
