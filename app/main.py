aa81764be95fbfca0e004654fb81553ff8815399
MakarosVar <christomvarv@yahoo.com>
2026-09-24 19:28:23 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ from app.api.error_handlers import (
     register_exception_handlers,
 )
 from app.api.routes.auth import router as auth_router
+from app.api.routes.permissions import router as permission_router
 from app.api.routes.roles import router as role_router
 from app.api.routes.users import router as user_router
 
@@ -25,3 +26,4 @@ allow_origins = [
 app.include_router(auth_router, prefix="/api")
 app.include_router(user_router, prefix="/api")
 app.include_router(role_router, prefix="/api")
+app.include_router(permission_router, prefix="/api")
